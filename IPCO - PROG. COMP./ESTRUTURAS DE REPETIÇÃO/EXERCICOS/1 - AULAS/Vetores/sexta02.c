f39d49e3d22240cde369b23a7a68935d7223f15a
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-03 21:38:03 -0300

    Create sexta02.c

---
+++ IPCO - PROG. COMP./ESTRUTURAS DE REPETIÇÃO/EXERCICOS/1 - AULAS/Vetores/sexta02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, num3, num4, num5;
+
+    printf("\n Digite o numero 1: \n");
+    scanf("%d", &num1);
+
+    printf("\n Digite o numero 2: \n");
+    scanf("%d", &num2);
+
+    printf("\n Digite o numero 3: \n");
+    scanf("%d", &num3);
+
+    printf("\n Digite o numero 4: \n");
+    scanf("%d", &num4);
+
+    printf("\n Digite o numero 5: \n");
+    scanf("%d", &num5);
+
+    printf("\n Numero 1: %d.\n", num1);
+    printf("\n Numero 2: %d.\n", num2);
+    printf("\n Numero 3: %d.\n", num3);
+    printf("\n Numero 4: %d.\n", num4);
+    printf("\n Numero 5: %d.\n", num5);
+
+    return (0);
+}
