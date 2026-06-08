c473cffa703ee683e69fb40d48014fcaa33d2dd9
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-08 17:20:44 -0300

    Delete IPCO - PROG. COMP. directory

--- IPCO - PROG. COMP./ESTRUTURAS DE REPETIÇÃO/EXERCICOS/1 - AULAS/Modularização de Arquivos/Ex2/mensagens.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void HelloWorld()
-{
-    printf("\n \nHello world!");
-}
-// void não retorna nada
--- IPCO - PROG. COMP./vetor01.c
+++
@@ -1,7 +0,0 @@
-#include "mensagens.c"
-
-int main()
-{
-    HelloWorld(); // eu imprimo, mas la no codigo esse valor ta solto
-    return 0;
-}
