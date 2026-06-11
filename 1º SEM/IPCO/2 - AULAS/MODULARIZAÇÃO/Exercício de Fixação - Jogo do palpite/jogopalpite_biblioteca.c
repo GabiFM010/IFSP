69184715b0ea8fab259917af9cfe5e211974f559
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 11:20:35 -0300

    Create jogopalpite_biblioteca.c

---
+++ 1º SEM/IPCO/2 - AULAS/MODULARIZAÇÃO/Exercício de Fixação - Jogo do palpite/jogopalpite_biblioteca.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+
+int gerar( int min , int max ){
+    srand((unsigned)time(NULL));
+    return( min + rand() %max );
+}
+
+float proximidadePalpite(f){
+    sub = n2 - n1;
+    return (prox = (sub / n1)*100);
+}
