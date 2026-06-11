b504830ad60515c430650ee52e78980346f8da20
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 11:46:13 -0300

    Update Ex1.c

--- 1º SEM/IPCO/2 - AULAS/VETORES/Exercícios de Fixação - Vetores/Ex1.c
+++ 1º SEM/IPCO/2 - AULAS/VETORES/Exercícios de Fixação - Vetores/Ex1.c
@@ -1,18 +1,24 @@
-/*
-    1 - Dado um vetor A definido como int A[10], preenchê-lo com os números inteiros 1,2,3, ...,10
-    gabrielly fernanda faria maia - tsi 026 - leal
+/* gabrielly fernanda faria maia
+   tsi 026 - leal - 06.05.2026
 */
+
+/*Exercicio 1. Dado um vetor A definido como int A[10], preenchê-lo com os números inteiros 1,2,3, ...,10*/
 
 #include <stdio.h>
 
-int main () 
-{
-    const int TAM = 10;
-    //int A [TAM];
+int main(){
 
-    for( int i=0; i<=TAM; i++){
-        printf("\n Numero é: %d. \n", i);
-    }
+   int TAM = 10; 
+   int num [TAM]; 
 
-    return (0);
+   for(int i=0; i<=TAM; i++){ 
+   printf("\n Digite o numero %d \n", i);
+   scanf("%d", &num[i]);
+   }
+
+   for(int i=0; i<=TAM; i++){
+   printf("\n Numero pos[%d]: %d", i, num[i]); // i = posição, num[i]=valor dentro da posição
+   }
+
+   return (0);
 }
