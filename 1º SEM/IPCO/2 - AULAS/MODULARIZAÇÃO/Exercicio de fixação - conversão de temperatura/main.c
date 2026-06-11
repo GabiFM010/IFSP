dccf0de26b4c437c9b64d3460757dd8eca336374
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 11:13:01 -0300

    Create main.c

---
+++ 1º SEM/IPCO/2 - AULAS/MODULARIZAÇÃO/Exercicio de fixação - conversão de temperatura/main.c
@@ -0,0 +1,12 @@
+// Objetivo: Criar uma biblioteca "converte" com funções para converter temperaturas (Celsius para Fahrenheit e vice-versa), distâncias (metros para pés e vice- versa) e pesos (quilogramas para libras e vice-versa).
+// Gabrielly Fernanda - TSI 026
+//  dia 15 de abril de 2026
+
+#include <stdio.h>
+
+int main()
+{
+
+    
+
+}
