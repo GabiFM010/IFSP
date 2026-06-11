24ae9b92f46a64b357d8a687e73eaced090a545d
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 11:43:55 -0300

    Create Ex1.c

---
+++ 1º SEM/IPCO/2 - AULAS/VETORES/Exercícios de Fixação - Vetores/Ex1.c
@@ -0,0 +1,20 @@
+/*
+    1 - Dado um vetor A definido como int A[10], preenchê-lo com os números inteiros 1,2,3, ...,10
+    gabrielly fernanda faria maia - tsi 026 - leal
+*/
+
+#include <stdio.h>
+
+
+
+int main () 
+{
+    const int TAM = 10;
+    //int A [TAM];
+
+    for( int i=0; i<=TAM; i++){
+        printf("\n Numero é: %d. \n", i);
+    }
+
+    return (0);
+}
