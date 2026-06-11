473ac6f17e5fc709325516c9038bd597550056c0
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 11:44:23 -0300

    Create Ex2.c

---
+++ 1º SEM/IPCO/2 - AULAS/VETORES/Exercícios de Fixação - Vetores/Ex2.c
@@ -0,0 +1,11 @@
+/*
+    1 - Dado um vetor B definido como int B[10], preenchê-lo com os números inteiros 10,9,8,…,1
+    Gabrielly fernanda faria maia - tsi 026 - leal
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    
+}
