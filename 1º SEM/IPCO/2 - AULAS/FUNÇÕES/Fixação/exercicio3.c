a51d6db1538c238656e1dc680c2a7362fd5522b1
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:24:19 -0300

    Create exercicio3.c

---
+++ 1º SEM/IPCO/2 - AULAS/FUNÇÕES/Fixação/exercicio3.c
@@ -0,0 +1,13 @@
+// Escreva uma função que determine o maior múltiplo de um inteiro dado menor do que ou igual a um outro inteiro dado. Por exemplo: o maior múltiplo de 7 menor que 50 é 49.
+
+#include <stdio.h>
+
+int main()
+{
+
+}
+
+int MaiorMultiplo(int inteiro, int maior)
+{
+
+}
