a854fc996c0b7eb9075403dac91c7a840955291e
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:14:47 -0300

    Create Ex6_1.c

---
+++ 1º SEM/IPCO/2 - AULAS/STRUCTS/Ex6_1.c
@@ -0,0 +1,51 @@
+/*
+    Codigo do professor - 10.06.2026
+    corrigi o codigo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// armazenar os dados de uma pessoa
+typedef struct{
+    char nome[30]; 
+    int idade; 
+    char sexo; 
+    char endereco[30]; 
+    char profissao[30]; 
+} Pessoa; 
+
+Pessoa lerDados();
+int exibirPessoa( Pessoa p);
+
+int main(){
+    Pessoa p1 = lerDados();
+    exibirPessoa( p1 );
+   return 0;
+}
+
+Pessoa lerDados(){
+   Pessoa p;
+   printf("Nome: ");
+   fgets(p.nome,30,stdin);
+   printf("Idade: ");
+   scanf("%d", &p.idade);
+   printf("Sexo: ");
+   scanf(" %c", &p.sexo);
+   while (getchar() != '\n'); //limpeza de buffer
+   printf("Endereco: ");
+   fgets(p.endereco,30,stdin);
+   printf("Profissao: ");
+   fgets(p.profissao,30,stdin);
+   return p;
+}
+
+int exibirPessoa( Pessoa p){
+    printf("\n Dados da pessoa");
+    printf("\n Nome: %s", p.nome);
+    printf("\n Idade: %d", p.idade);
+    printf("\n Sexo: %c", p.sexo);
+    printf("\n Endereco: %s", p.endereco);
+    printf("\n Profissao: %s", p.profissao);
+};
