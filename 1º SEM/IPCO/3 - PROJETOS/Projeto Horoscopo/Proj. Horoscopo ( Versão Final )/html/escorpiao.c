288bc2bfe0d3175dcb0ad3b7b85fa97e91ca4822
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 12:01:02 -0300

    Create escorpiao.c

---
+++ 1º SEM/IPCO/3 - PROJETOS/Projeto Horoscopo/Proj. Horoscopo ( Versão Final )/html/escorpiao.c
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Escorpião </title>
+</head>
+<body>
+
+    <main>
+        <h1> Escorpião </h1>
+            <img src="img/escorpiao.png" alt="Escorpião" width="600px">
+    </main>
+
+</body>
+</html>
