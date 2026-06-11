86cbb5714d4ce3f777c38e210edf33efa48a2bf5
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:24:41 -0300

    Create exercicio4.c

---
+++ 1º SEM/IPCO/2 - AULAS/FUNÇÕES/Fixação/exercicio4.c
@@ -0,0 +1,8 @@
+// Escreva um programa que calcule o valor de uma média ponderada, considerando duas notas e dois pesos informados pelo usuário. O cálculo da média deve ser feito usando uma função.
+
+#include <stdio.h>
+
+int main()
+{
+    
+}
