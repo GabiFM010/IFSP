e52fedef81600836b7b554e03465958ee8744707
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:17:19 -0300

    Create ex2.c

---
+++ 1º SEM/IPCO/2 - AULAS/MATRIZ/ex2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define LINHA 2
+#define COLUNA 3
+
+int main(){
+
+    // exemplo de definição de matriz 
+    
+    int mat /*x*/[LINHA]/*y*/[COLUNA];
+
+    // leitura dos valores
+        for (int i=0; i < LINHA; i++){
+            for (int j=0; j < COLUNA; j++){
+                printf("informe a mat(riz) [%d][%d]: ", i, j);
+                scanf("%d", &mat[i][j]);
+            }
+        }
+    // exibição dos valores
+        printf("Os dados armazenados na matriz são: \n");
+            for (int i=0; i< LINHA; i++){
+                for (int j=0; j < COLUNA; j++){
+                    printf("%d", mat[i][j]); //exibe o que ta no indice daquela repetição de linha e coluna
+                }
+                printf("\n");
+            }
+
+    return(0);
+}
