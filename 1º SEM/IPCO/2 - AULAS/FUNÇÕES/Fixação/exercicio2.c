1411dcb596c49ef637dd5ee40be50590104a1442
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:23:59 -0300

    Create exercicio2.c

---
+++ 1º SEM/IPCO/2 - AULAS/FUNÇÕES/Fixação/exercicio2.c
@@ -0,0 +1,34 @@
+// Considere que uma calculadora comum, de quatro operações aritméticas, está com as teclas de divisão
+// e multiplicação quebradas. Escreva funções para a multiplicação e divisão, usando apenas as operações de adição e subtração.
+// Gabrielly Fernanda - TSI 026
+// objetivo: programa que contem funções de multiplicação e divisão de uma calculadora com esssas mesmas teclas quebradas
+
+#include <stdio.h>
+
+int soma();
+int subtracao();
+int multiplicacao();
+int divisao();
+
+int main()
+{
+    int operacao;
+    int n1 = 12, n2 = 4;
+
+    
+
+}
+
+int multiplicacao(int a, int b)
+{
+    int resultado = 0;
+    for (int i = 1; i <= b; i++){
+        resultado += a;;
+    }
+    return (resutado);
+}
+
+int divisao(int a, int b)
+{
+    if (b == )
+}
