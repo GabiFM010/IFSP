a8811876d95a8e217b43a43604aaafeb7faf1214
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 12:00:41 -0300

    Create capricornio.c

---
+++ 1º SEM/IPCO/3 - PROJETOS/Projeto Horoscopo/Proj. Horoscopo ( Versão Final )/html/capricornio.c
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Capricórnio </title>
+</head>
+<body>
+
+    <main>
+        <h1> Capricórnio </h1>
+            <img src="img/carpicornio.png" alt="Capricórnio" width="600px">
+    </main>
+
+</body>
+</html>
