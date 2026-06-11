ce4c929ea2e5c63f753a84e46cad1f5b67ce30e7
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-06-11 09:21:53 -0300

    Create Ex1.c

---
+++ 1º SEM/IPCO/2 - AULAS/FUNÇÕES/Ex1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){ // trecho de codigo de nome main, int é o retorno, garante o retorno inteiro
+    printf("Hello world!"); //prinf: função do stdio.h, ja feita nessa biblioteca, função de exibição, ja preparada para receber diferentes argumentos
+    return(0); //retorno do int main
+}
