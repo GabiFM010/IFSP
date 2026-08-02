a4d5b3c4f24fbe2b5232316ff5d2cf21c2dbf0ca
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-08-02 17:12:31 -0300

    Delete 2º SEM/INOO/1 - AULAS directory

--- 2º SEM/INOO/1 - AULAS/Ola.java
+++
@@ -1,5 +0,0 @@
-public class Ola{
-    public static void main(String[] args) { // args: argumento
-        System.out.println("Olá mundo!"); // system é uma classe representante do s.o, o ponto se refere ao s.o que quer uma saida
-    }
-}
