7d4427a38fd72b27cf62964421d558d9863c0f98
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-08-07 21:37:05 -0300

    Add files via upload

---
+++ 2º SEM/INOO/2 - AULAS/aula 2/Chama.java
@@ -0,0 +1,9 @@
+public class Chama{
+    public static void main(String[] args) {
+        System.out.println("
+            Long.MAX_VALUE: " + Long.MAX_VALUE);
+        System.out.println("Ticks: " + System.currentTimeMillis());
+
+// 32 bits ta valendo 2038    
+    }
+}
