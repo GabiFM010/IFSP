799f4db4943d52a5f3bc7e5851c0e1e6d128c874
Gabrielly Fernanda <168764074+GabiFM010@users.noreply.github.com>
2026-08-21 22:00:39 -0300

    Add files via upload

---
+++ 2º SEM/INOO/2 - AULAS/aula 4/exercicio moodle/ex1/Produto.java
@@ -0,0 +1,20 @@
+/*
+    Exercício 1
+    Defina uma classe Produto (código : inteiro; descrição : texto; preço : decimal).
+    A classe deve ter:
+    Métodos get/set para acessar e modificar os valores dos atributos.
+    Um construtor padrão (sem argumentos).
+    Um construtor que receba os valores de cada um dos argumentos.
+    Use a palavra reservada this, quando for o caso.
+    Defina também uma classe ProdutoDemo para testar o funcionamento da classe Produto.
+*/
+
+public class Produto {
+    private int codigo;
+    private String descricao;
+    private double preco;
+
+
+
+
+}
